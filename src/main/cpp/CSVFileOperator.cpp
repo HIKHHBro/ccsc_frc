2b1250a92e190de0b96c6fad7f6b895b9a0a8398
HIKHHBro <w1121508831@gmail.com>
2021-01-19 19:32:18 +0800

    注释掉csv

--- src/main/cpp/CSVFileOperator.cpp
+++ src/main/cpp/CSVFileOperator.cpp
@@ -1,25 +1,25 @@
-#include "CSVFileOperator.h"
+// #include "CSVFileOperator.h"
 
-CSVFileOperator::CSVFileOperator(/* args */)
-{
-}
+// CSVFileOperator::CSVFileOperator(/* args */)
+// {
+// }
 
-CSVFileOperator::~CSVFileOperator()
-{
-}
+// CSVFileOperator::~CSVFileOperator()
+// {
+// }
 
-bool CSVFileOperator::write_data(std::string,float data)
-{
+// bool CSVFileOperator::write_data(std::string,float data)
+// {
 
-	std::ofstream outFile;
-	outFile.open("data.csv", ios::out); // 打开模式可省略
-	outFile << "name" << ',' << "age" << ',' << "hobby" << endl;
+// 	std::ofstream outFile;
+// 	outFile.open("data.csv", ios::out); // 打开模式可省略
+// 	outFile << "name" << ',' << "age" << ',' << "hobby" << endl;
 
-}
+// }
 
 
-	private static final String kFileName = "canlog.csv";
+// 	private static final String kFileName = "canlog.csv";
 
-	private static final Path sCsvFile = RobotBase.isReal()
-			? Paths.get("/home/lvuser", kFileName)
-			: Paths.get(Filesystem.getOperatingDirectory().toString(), kFileName);
+// 	private static final Path sCsvFile = RobotBase.isReal()
+// 			? Paths.get("/home/lvuser", kFileName)
+// 			: Paths.get(Filesystem.getOperatingDirectory().toString(), kFileName);
--- src/main/include/CSVFileOperator.h
+++ src/main/include/CSVFileOperator.h
@@ -1,25 +1,25 @@
-#ifndef __CSVF_H
-#define __CSVF_H
-#include <iostream>
-#include <string>
-#include <vector>
-#include <fstream>
-#include <sstream>
+// #ifndef __CSVF_H
+// #define __CSVF_H
+// #include <iostream>
+// #include <string>
+// #include <vector>
+// #include <fstream>
+// #include <sstream>
 
-class CSVFileOperator
-{
-private:
-	static std::string name[];
-	std::string kFileName = "config.csv";
+// class CSVFileOperator
+// {
+// private:
+// 	static std::string name[];
+// 	std::string kFileName = "config.csv";
 
-public:
-	CSVFileOperator(/* args */);
-	~CSVFileOperator();
-	static bool write_data(std::string,float data);
-	static bool write_data(std::string,int data);
-};
-
-
+// public:
+// 	CSVFileOperator(/* args */);
+// 	~CSVFileOperator();
+// 	static bool write_data(std::string,float data);
+// 	static bool write_data(std::string,int data);
+// };
 
 
-#endif
+
+
+// #endif
