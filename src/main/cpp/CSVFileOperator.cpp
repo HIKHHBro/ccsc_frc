de0d004903ea1ad9ff749ac55fd56115afbf1fbb
HIKHHBro <w1121508831@gmail.com>
2021-01-20 19:29:49 +0800

    注释csv

--- src/main/cpp/CSVFileOperator.cpp
+++ src/main/cpp/CSVFileOperator.cpp
@@ -1,4 +1,30 @@
-<<<<<<< HEAD
+// <<<<<<< HEAD
+// // #include "CSVFileOperator.h"
+
+// // CSVFileOperator::CSVFileOperator(/* args */)
+// // {
+// // }
+
+// // CSVFileOperator::~CSVFileOperator()
+// // {
+// // }
+
+// // bool CSVFileOperator::write_data(std::string,float data)
+// // {
+
+// // 	std::ofstream outFile;
+// // 	outFile.open("data.csv", ios::out); // 打开模式可省略
+// // 	outFile << "name" << ',' << "age" << ',' << "hobby" << endl;
+
+// // }
+
+
+// // 	private static final String kFileName = "canlog.csv";
+
+// // 	private static final Path sCsvFile = RobotBase.isReal()
+// // 			? Paths.get("/home/lvuser", kFileName)
+// // 			: Paths.get(Filesystem.getOperatingDirectory().toString(), kFileName);
+// =======
 // #include "CSVFileOperator.h"
 
 // CSVFileOperator::CSVFileOperator(/* args */)
@@ -24,30 +50,4 @@
 // 	private static final Path sCsvFile = RobotBase.isReal()
 // 			? Paths.get("/home/lvuser", kFileName)
 // 			: Paths.get(Filesystem.getOperatingDirectory().toString(), kFileName);
-=======
-#include "CSVFileOperator.h"
-
-CSVFileOperator::CSVFileOperator(/* args */)
-{
-}
-
-CSVFileOperator::~CSVFileOperator()
-{
-}
-
-bool CSVFileOperator::write_data(std::string,float data)
-{
-
-	std::ofstream outFile;
-	outFile.open("data.csv", ios::out); // 打开模式可省略
-	outFile << "name" << ',' << "age" << ',' << "hobby" << endl;
-
-}
-
-
-	private static final String kFileName = "canlog.csv";
-
-	private static final Path sCsvFile = RobotBase.isReal()
-			? Paths.get("/home/lvuser", kFileName)
-			: Paths.get(Filesystem.getOperatingDirectory().toString(), kFileName);
->>>>>>> 662fd7ce2374ed2e6002e4c602037dee8fea27ec
+// >>>>>>> 662fd7ce2374ed2e6002e4c602037dee8fea27ec
